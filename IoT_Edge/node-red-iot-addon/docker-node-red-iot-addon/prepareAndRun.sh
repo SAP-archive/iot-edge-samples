d631c8c8879fbabb0a5b13003c8de56302817f75
Marco Porru <marco.porru@sap.com>
2021-11-29 12:30:36 +0100

    fixed noderediotaddon
    Signed-off-by: Marco Porru <marco.porru@sap.com>

--- IoT_Edge/node-red-iot-addon/docker-node-red-iot-addon/prepareAndRun.sh
+++ IoT_Edge/node-red-iot-addon/docker-node-red-iot-addon/prepareAndRun.sh
@@ -2,19 +2,21 @@
 
 declare -i USER_FATAL_ERROR=192
 declare -i USER_ALL_OK=0
-declare -i EDGE_GATEWAY_HOST=edge-gateway-service.sap-iot-gateway.svc.cluster.local
-declare -i PERSISTENCE=persistence-service.persistence-service.svc.cluster.local
+declare -i EDGE_GATEWAY_HOST=edge-gateway-service.sap-iot-gateway
+declare -i PERSISTENCE=persistence-service.persistence-service
+
+NODERED_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
 
 echo -e "PERSISTENCE URL=${PERSISTENCE}"
+echo -e "NODERED NAMESPACE=${NODERED_NAMESPACE}"
 echo -e "EDGE_GATEWAY_MQTT_BUS_TCP_PORT=${EDGE_GATEWAY_MQTT_BUS_TCP_PORT}"
+echo -e "EDGE_GATEWAY_MQTT_INGESTION_PORT=${EDGE_GATEWAY_INGESTION_PORT}"
 echo -e "EDGE_GATEWAY_REST_API_TCP_PORT=${EDGE_GATEWAY_REST_API_TCP_PORT}"
 
-sed  -i -e 's@<<PERSISTENCE>>@'"$PERSISTENCE"'@g' /data/flows-rep.json
-sed  -i -e 's@<<EDGEGATEWAY>>@'"$EDGE_GATEWAY_HOST"'@g' /data/flows-rep.json
-sed  -i -e 's@<<EDGEGATEWAY_BUS>>@'"$EDGE_GATEWAY_MQTT_BUS_TCP_PORT"'@g' /data/flows-rep.json
+sed  -i -e 's@<<NAMESPACE>>@'"$NODERED_NAMESPACE"'@g' /data/flows-rep.json
+sed  -i -e 's@<<EDGEGATEWAYINGESTION>>@'"$EDGE_GATEWAY_INGESTION_PORT"'@g' /data/flows-rep.json
 sed  -i -e 's@<<EDGERESTPORT>>@'"$EDGE_GATEWAY_REST_API_TCP_PORT"'@g' /data/flows-rep.json
 sed  -i -e 's@<<EDGEBUSPORT>>@'"$EDGE_GATEWAY_MQTT_BUS_TCP_PORT"'@g' /data/flows-rep.json
-#sed  -i -e 's@<<EDGEGATEWAY_INGESTION>>@'"$EDGE_GATEWAY_HOST"'@g' test
 
 npm start --cache /data/.npm -- --userDir /data
 
